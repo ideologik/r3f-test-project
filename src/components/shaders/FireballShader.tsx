b3cf885628b47a3ffe3bdb01fc969b173fc489da
Jorge Fox D'Angelo <jgdfox@gmail.com>
2023-10-02 07:56:54 -0300

    fix const not used

--- src/components/shaders/FireballShader.tsx
+++ src/components/shaders/FireballShader.tsx
@@ -63,55 +63,55 @@ const FlameShader = () => {
   
   
   `;
-  const fragmentShader3d = `
+  // const fragmentShader3d = `
 
-  precision highp float;
-  
-  uniform float iTime;
-  uniform vec2 iResolution;
-  uniform vec2 iMouse;
-  
-  #define fragColor gl_FragColor
-  
-  mat2 rotate2D(float r) {
-      return mat2(cos(r), sin(r), -sin(r), cos(r));
-  }
-  
-  void main() {
-      vec2 uv = (gl_FragCoord.xy - 0.5 * iResolution.xy) / iResolution.y;
+  // precision highp float;
 
-      vec3 ro = vec3(0, 0, 0); 
-      vec3 rd = normalize(vec3(uv, 1.0)); 
+  // uniform float iTime;
+  // uniform vec2 iResolution;
+  // uniform vec2 iMouse;
 
-      vec3 col = vec3(0);
-      float t = iTime * 0.00001;
+  // #define fragColor gl_FragColor
 
-      float angle = iMouse.x * 3.14; 
-      mat2 rot = mat2(cos(angle), -sin(angle), sin(angle), cos(angle));
-      vec2 uvRotated = uv * rot;
-      
-      vec2 n = vec2(0);
-      vec2 q = vec2(0.0);
-      vec2 p = uvRotated * rd.xy;  // Aquí usamos la dirección del rayo en el cálculo.
-      float d = dot(p, p);
-      float S = 12.0;
-      float a = 0.0;
-      mat2 m = rotate2D(5.);
-      
-      for (float j = 0.; j < 20.0; j++) {
-          p *= m;
-          n *= m;
-          q = p * S + t * 5. + sin(t * 60. - d * 4.) * 1. + j + n;
-          a += dot(cos(q) / S, vec2(0.2));
-          n -= sin(q + iTime);
-          S *= 1.2;
-      }
+  // mat2 rotate2D(float r) {
+  //     return mat2(cos(r), sin(r), -sin(r), cos(r));
+  // }
 
-      col = vec3(4, 2, 1) * (a + 0.2) + a + a - d;
-      fragColor = vec4(col, 1.0);
-  }
-  
-  `;
+  // void main() {
+  //     vec2 uv = (gl_FragCoord.xy - 0.5 * iResolution.xy) / iResolution.y;
+
+  //     vec3 ro = vec3(0, 0, 0);
+  //     vec3 rd = normalize(vec3(uv, 1.0));
+
+  //     vec3 col = vec3(0);
+  //     float t = iTime * 0.00001;
+
+  //     float angle = iMouse.x * 3.14;
+  //     mat2 rot = mat2(cos(angle), -sin(angle), sin(angle), cos(angle));
+  //     vec2 uvRotated = uv * rot;
+
+  //     vec2 n = vec2(0);
+  //     vec2 q = vec2(0.0);
+  //     vec2 p = uvRotated * rd.xy;  // Aquí usamos la dirección del rayo en el cálculo.
+  //     float d = dot(p, p);
+  //     float S = 12.0;
+  //     float a = 0.0;
+  //     mat2 m = rotate2D(5.);
+
+  //     for (float j = 0.; j < 20.0; j++) {
+  //         p *= m;
+  //         n *= m;
+  //         q = p * S + t * 5. + sin(t * 60. - d * 4.) * 1. + j + n;
+  //         a += dot(cos(q) / S, vec2(0.2));
+  //         n -= sin(q + iTime);
+  //         S *= 1.2;
+  //     }
+
+  //     col = vec3(4, 2, 1) * (a + 0.2) + a + a - d;
+  //     fragColor = vec4(col, 1.0);
+  // }
+
+  // `;
 
   const mouseRef = useRef(new Vector2(0.5, 0.5));
 
